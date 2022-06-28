c6fb1481d1f69465372cccef317959b1a14eea11
Roberto <rpgrca@gmail.com>
2022-06-27 22:22:46 -0300

    add test, character returns birthday correctly

--- MiniRpg2022.Logic/Character.cs
+++ MiniRpg2022.Logic/Character.cs
@@ -3,10 +3,12 @@ namespace Videogame;
 public class Character
 {
     public string Name { get; }
+    public DateOnly Birthday { get; }
 
-    public Character(string name)
+    public Character(string name, DateOnly birthday)
     {
         if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Invalid name", nameof(name));
         Name = name;
+        Birthday = birthday;
     }
 }
--- MiniRpg2022.UnitTests/CharacterMust.cs
+++ MiniRpg2022.UnitTests/CharacterMust.cs
@@ -4,10 +4,12 @@ namespace MiniRpg2022.UnitTests;
 
 public class CharacterMust
 {
+    private static readonly DateOnly RAISTLIN_BIRTHDAY = new DateOnly(2017, 3, 15);
+
     [Fact]
     public void ReturnNameCorrectly()
     {
-        var sut = new Character("Raistlin");
+        var sut = new Character("Raistlin", RAISTLIN_BIRTHDAY);
         Assert.Equal("Raistlin", sut.Name);
     }
 
@@ -17,8 +19,15 @@ public class CharacterMust
     [InlineData(null)]
     public void ThrowException_WhenNameIsInvalid(string invalidName)
     {
-        var exception = Assert.Throws<ArgumentException>("name", () => new Character(invalidName));
+        var exception = Assert.Throws<ArgumentException>("name", () => new Character(invalidName, RAISTLIN_BIRTHDAY));
         Assert.StartsWith("Invalid name", exception.Message);
         Assert.Contains("'name'", exception.Message);
+    }
+
+    [Fact]
+    public void ReturnBirthdayCorrectly()
+    {
+        var sut = new Character("Raistlin", RAISTLIN_BIRTHDAY);
+        Assert.Equal(RAISTLIN_BIRTHDAY, sut.Birthday);
     }
 }
