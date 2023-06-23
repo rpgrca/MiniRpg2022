45c7419cc1eed3e436246d486908d4e3222f5da9
Roberto <rpgrca@gmail.com>
2023-06-22 22:32:55 -0300

    add test, return true when quit menu gets Y

---
+++ MiniRpg2022.Console.UnitTests/QuitMenuMust.cs
@@ -0,0 +1,26 @@
+﻿using MiniRpg2022.Console;
+using Xunit;
+
+namespace MiniRpg2022.Console.UnitTests;
+
+public class QuitMenuMust
+{
+    public class TestableQuitMenu : QuitMenu
+    {
+        private string? _readText;
+        public string WrittenText { get; private set; }
+
+        public TestableQuitMenu(string? readText) => _readText = readText;
+
+        protected override void WriteToConsole(string text) => WrittenText = text;
+
+        protected override string? ReadLineFromConsole() => _readText;
+    }
+
+    [Fact]
+    public void ReturnTrue_WhenQuitMenuReadsY()
+    {
+        var sut = new TestableQuitMenu("Y");
+        Assert.True(sut.Execute(null));
+    }
+}
