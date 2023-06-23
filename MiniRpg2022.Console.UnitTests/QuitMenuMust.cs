933b1190485487b4b985dbba445f4f9db8b0c1fa
Roberto <rpgrca@gmail.com>
2023-06-23 16:25:50 -0300

    use standard configuration instead of null

--- MiniRpg2022.Console.UnitTests/QuitMenuMust.cs
+++ MiniRpg2022.Console.UnitTests/QuitMenuMust.cs
@@ -1,5 +1,6 @@
 ﻿using MiniRpg2022.Console;
 using System.Collections.Generic;
+using Videogame;
 using Xunit;
 
 namespace MiniRpg2022.Console.UnitTests;
@@ -8,11 +9,14 @@ public class QuitMenuMust
 {
     public class TestableQuitMenu : QuitMenu
     {
-        private string? _readText;
+        private readonly string[] _readText;
+        private int _index;
+
         public List<string> WrittenText { get; private set; }
 
-        public TestableQuitMenu(string? readText)
+        public TestableQuitMenu(string[] readText)
         {
+            _index = 0;
             _readText = readText;
             WrittenText = new List<string>();
         }
@@ -20,28 +24,31 @@ public class QuitMenuMust
         protected override void WriteToConsole(string text) =>
             WrittenText.Add(text);
 
-        protected override string? ReadLineFromConsole() => _readText;
+        protected override string? ReadLineFromConsole() => _readText[_index++];
     }
 
     [Fact]
     public void ReturnTrue_WhenQuitMenuReadsY()
     {
-        var sut = new TestableQuitMenu("Y");
-        Assert.True(sut.Execute(null));
+        var sut = new TestableQuitMenu(new[] { "Y" });
+        Assert.True(sut.Execute(ObtenerConfiguracion()));
     }
+
+    private static Configuration ObtenerConfiguracion() => new(2022, 11, 30);
 
     [Fact]
     public void ReturnFalse_WhenQuitMenuReadsN()
     {
-        var sut = new TestableQuitMenu("N");
-        Assert.False(sut.Execute(null));
+        var sut = new TestableQuitMenu(new[] { "N" });
+        Assert.False(sut.Execute(ObtenerConfiguracion()));
     }
 
     [Fact]
     public void DisplayCorrectPrompt()
     {
-        var sut = new TestableQuitMenu("Y");
-        sut.Execute(null);
+        var sut = new TestableQuitMenu(new[] { "Y" });
+        sut.Execute(ObtenerConfiguracion());
         Assert.Collection(sut.WrittenText, p => Assert.Equal("- Are you sure? [Y, N]: ", p));
     }
+
 }
